8d0ac8973df751945b9dafb941bac839c0e1a207
Saurabh Singh <singhsaurabh162000@gmail.com>
2022-09-06 11:55:10 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <h1>Initial Check</h1>
+      <Footer />
     </div>
   );
 }
