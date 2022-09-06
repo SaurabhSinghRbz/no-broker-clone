43ffada7c08d7462fb4f458771a35b8eff484438
Yash Verma <yashv1752@gmail.com>
2022-09-06 15:06:26 +0530

    Result shower Added

---
+++ src/Components/SearchPages/ResultShower.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+
+export const ResultShower = () => {
+
+    const [age, setAge] = React.useState('');
+
+    console.log(age)
+
+
+    return (
+        <div style={{backgroundColor:'white',padding:'1%',marginTop:'1%',borderTop:'0.1px solid gainsboro'}}>
+            <div>
+                <span style={{fontSize:'90%',fontWeight:'300'}}>Home / Delhi / Telibara</span>
+                <span style={{marginLeft:'50%', fontSize:'90%',fontWeight:'300'}}>
+                    Sort By:
+                    <Select
+                        value={age}
+                        style={{width:'9rem',marginLeft:'1%',border:'none'}}
+                        label=''
+                        onChange={(e)=>setAge(e.target.value)}
+                    >
+                        <MenuItem value={'increasing'}>Low to High</MenuItem>
+                        <MenuItem value={'decreasing'}>High to Low</MenuItem>
+                        <MenuItem value={''}>Clear</MenuItem>
+                    </Select>
+                </span>
+            </div>
+            <div style={{fontSize:'110%'}}>112 - Flats, Apartments for Rent in Telibara, Delhi | Rental Properties in Telibara</div>
+        </div>
+    )
+}
--- src/Pages/SearchPage/SearchPage.jsx
+++ src/Pages/SearchPage/SearchPage.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import '../../Styles/SearchPages/searchpage.css'
 import { DataCard } from '../../Components/SearchPages/DataCard'
+import { ResultShower } from '../../Components/SearchPages/ResultShower';
 
 export const SearchPage = () => {
     const array = ['data1','data2','data3','data4','data5','data6'];
 
   return (
     <div className='search-page-head-div'>
-        <div></div>
         <div>
+        </div>
+        <div>
+            <ResultShower />
             {
                 array.map(()=>{
                     return(
