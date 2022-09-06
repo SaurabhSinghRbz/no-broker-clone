b529a3cef20cf6ff4fbe82427c39675c4180d39a
Saurabh Singh <singhsaurabh162000@gmail.com>
2022-09-06 16:34:34 +0530

    footer comercial part2 completed

--- src/Components/Footer/FooterSubOptions6.jsx
+++ src/Components/Footer/FooterSubOptions6.jsx
@@ -53,39 +53,96 @@ function FooterSubOptions6() {
         "Commercial Shops for Rent in Gurgaon",
     ];
 
+    const textArr6 = [
+        "Office Space for Sale in Bangalore", "Industrial Building for Sale in Bangalore", "Coworking Spaces for Sale in Bangalore", "Industrial Shed for Sale in Bangalore", "Commercial Shops for Sale in Bangalore", "Warehouse for Sale in Bangalore", "Commercial Showrooms for Sale in Bangalore"
+    ]
+
+    const textArr7 = [
+        "Office Space for Sale in Chennai", "Industrial Building for Sale in Chennai", "Coworking Spaces for Sale in Chennai", "Industrial Shed for Sale in Chennai", "Commercial Shops for Sale in Chennai", "Warehouse for Sale in Chennai", "Commercial Showrooms for Sale in Chennai"
+    ]
+
+    const textArr8 = [
+        "Coworking Spaces for Sale in Pune", "Industrial Shed for Sale in Pune", "Industrial Building for Sale in Pune", "Commercial Shops for Sale in Pune", "Warehouse for Sale in Pune", "Commercial Showrooms for Sale in Pune", "Office Space for Sale in Pune"
+    ]
+
+    const textArr9 = [
+        "Industrial Building for Sale in Mumbai", "Coworking Spaces for Sale in Mumbai", "Commercial Shops for Sale in Mumbai", "Industrial Shed for Sale in Mumbai", "Commercial Showrooms for Sale in Mumbai", "Warehouse for Sale in Mumbai", "Office Space for Sale in Mumbai"
+    ]
+
+    const textArr10 = [
+        "Coworking Spaces for Sale in Gurgaon", "Industrial Shed for Sale in Gurgaon", "Commercial Shops for Sale in Gurgaon", "Commercial Showrooms for Sale in Gurgaon", "Warehouse for Sale in Gurgaon", "Office Space for Sale in Gurgaon", "Industrial Building for Sale in Gurgaon"
+    ]
+
+
     return (
-        <Flex className={style.footerSubOptions}>
-            <Box>
-                <Box>Commercial Properties For Rent in Bangalore</Box>
-                {textArr1.map((text) => (
-                    <FooterSubOptionsText text={text} />
-                ))}
-            </Box>
-            <Box>
-                <Box>Commercial Properties For Rent in Chennai</Box>
-                {textArr2.map((text) => (
-                    <FooterSubOptionsText text={text} />
-                ))}
-            </Box>
-            <Box>
-                <Box>Commercial Properties For Rent in Pune</Box>
-                {textArr3.map((text) => (
-                    <FooterSubOptionsText text={text} />
-                ))}
-            </Box>
-            <Box>
-                <Box>Commercial Properties For Rent in Mumbai</Box>
-                {textArr4.map((text) => (
-                    <FooterSubOptionsText text={text} />
-                ))}
-            </Box>
-            <Box>
-                <Box>Commercial Properties For Rent in Gurgaon</Box>
-                {textArr5.map((text) => (
-                    <FooterSubOptionsText text={text} />
-                ))}
-            </Box>
-        </Flex>
+        <Box>
+            <Flex className={style.footerSubOptions}>
+                <Box>
+                    <Box>Commercial Properties For Rent in Bangalore</Box>
+                    {textArr1.map((text) => (
+                        <FooterSubOptionsText text={text} />
+                    ))}
+                </Box>
+                <Box>
+                    <Box>Commercial Properties For Rent in Chennai</Box>
+                    {textArr2.map((text) => (
+                        <FooterSubOptionsText text={text} />
+                    ))}
+                </Box>
+                <Box>
+                    <Box>Commercial Properties For Rent in Pune</Box>
+                    {textArr3.map((text) => (
+                        <FooterSubOptionsText text={text} />
+                    ))}
+                </Box>
+                <Box>
+                    <Box>Commercial Properties For Rent in Mumbai</Box>
+                    {textArr4.map((text) => (
+                        <FooterSubOptionsText text={text} />
+                    ))}
+                </Box>
+                <Box>
+                    <Box>Commercial Properties For Rent in Gurgaon</Box>
+                    {textArr5.map((text) => (
+                        <FooterSubOptionsText text={text} />
+                    ))}
+                </Box>
+            </Flex>
+
+            <Flex className={style.footerSubOptions}>
+                <Box>
+                    <Box>Commercial Properties For Sale in Bangalore</Box>
+                    {textArr6.map((text) => (
+                        <FooterSubOptionsText text={text} />
+                    ))}
+                </Box>
+                <Box>
+                    <Box>Commercial Properties For Sale in Chennai</Box>
+                    {textArr7.map((text) => (
+                        <FooterSubOptionsText text={text} />
+                    ))}
+                </Box>
+                <Box>
+                    <Box>Commercial Properties For Sale in Pune</Box>
+                    {textArr8.map((text) => (
+                        <FooterSubOptionsText text={text} />
+                    ))}
+                </Box>
+                <Box>
+                    <Box>Commercial Properties For Sale in Mumbai</Box>
+                    {textArr9.map((text) => (
+                        <FooterSubOptionsText text={text} />
+                    ))}
+                </Box>
+                <Box>
+                    <Box>Commercial Properties For Sale in Gurgaon</Box>
+                    {textArr10.map((text) => (
+                        <FooterSubOptionsText text={text} />
+                    ))}
+                </Box>
+            </Flex>
+        </Box>
+
     );
 }
 
