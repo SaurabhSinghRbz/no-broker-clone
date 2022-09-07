6614815e5f0d53d5955fb318ef11b248c68d041c
Yash Verma <yashv1752@gmail.com>
2022-09-07 09:13:03 +0530

    searchbar

--- src/Pages/SearchPage/SearchPage.jsx
+++ src/Pages/SearchPage/SearchPage.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import '../../Styles/SearchPages/searchpage.css'
 import { DataCard } from '../../Components/SearchPages/DataCard'
 import { ResultShower } from '../../Components/SearchPages/ResultShower';
+import { SearchBar } from '../../Components/SearchPages/SearchBar';
 
 export const SearchPage = () => {
     const array = ['data1','data2','data3','data4','data5','data6'];
 
+
+
   return (
+    <div>
+    <SearchBar />    
     <div className='search-page-head-div'>
         <div>
         </div>
@@ -24,6 +29,7 @@ export const SearchPage = () => {
             }
         </div>
         <div></div>
+    </div>
     </div>
   )
 }
