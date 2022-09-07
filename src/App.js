703fc794d49209339db50703cf99da91b178a035
Saurabh Singh <singhsaurabh162000@gmail.com>
2022-09-07 10:06:43 +0530

    initil commit for homepage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 import './App.css';
 import Footer from './Components/Footer/Footer';
 import Navbar from './Components/Navbar/Navbar';
+import Homepage from './Pages/Homepage/Homepage';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Homepage />
       <div style={{ height: "500px" }}></div>
+      <h1></h1>
       <Footer />
     </div>
   );
---
+++ src/Pages/Homepage/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from "./Homepage.module.css"
+
+import { Box } from "@chakra-ui/react"
+
+function Homepage() {
+    return (
+        <Box>Homepage</Box>
+    )
+}
+
+export default Homepage
