0d1204bc63195c6b85b84078d823e27a60561823
Akash Jadhav <akashjadhav7040253178@gmail.com>
2022-09-09 15:38:11 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import './App.css';
-import ToPayment from './Components/Card_Detail_Page/payment';
 import Footer from './Components/Footer/Footer';
 import Navbar from './Components/Navbar/Navbar';
-import Payment from './Components/Payment_Detail/payment';
-import Rent from './Components/Rent_Page/rent';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      {/* <Rent/> */}
-      {/* <Payment/> */}
-      <ToPayment/>
+
     </div>
   );
 }
