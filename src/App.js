5f3ca542436bca492f50c70b45f772be2d9e0a92
Saurabh Singh <singhsaurabh162000@gmail.com>
2022-09-09 10:03:19 +0530

    login-signup routing working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 
+import { Button } from '@chakra-ui/react';
+import React from 'react';
+import { Link, Route, Routes, useParams } from 'react-router-dom';
 import './App.css';
 import Footer from './Components/Footer/Footer';
 import Navbar from './Components/Navbar/Navbar';
 import Homepage from './Pages/Homepage/Homepage';
+import LoginSignup from './Pages/LoginSignup/LoginSignup';
 
 function App() {
+  const id = useParams()
+  console.log(id)
   return (
     <div className="App">
+      {/* <Link to="/login"><Button>Click ME</Button></Link> */}
       <Navbar />
       <Homepage />
       <Footer />
+      <Routes>
+        <Route path="/login-signup" element={<LoginSignup />} />
+      </Routes>
+      {/* <LoginSignup /> */}
     </div>
   );
 }
---
+++ src/Pages/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import style from "./LoginSignup.module.css"
+import { Box, Text, Flex, Image, List, ListItem, ListIcon, CloseButton } from "@chakra-ui/react"
+import { BsCheck } from "react-icons/bs"
+import signupHome from "../../Images/Login/signupHome.png"
+import { Navigate } from 'react-router-dom'
+
+
+function LoginSignup() {
+    const [show, setShow] = React.useState(true)
+    if (!show) {
+        return <Navigate to='/' />
+    }
+    return show && (
+        <Box className={style.loginSignupContainer}>
+            <Box>
+                <Box h="100%" position="relative">
+                    <Box>
+                        <CloseButton
+                            alignSelf='flex-start'
+                            position='relative'
+                            right={-1}
+                            top={-1}
+                            onClick={() => setShow(false)}
+                        />
+                    </Box>
+
+                    <Flex flexDirection="row" w="100%" h="100%">
+                        <Flex className={style.loginLeftBox}>
+                            <Box ml="10%" alignSelf="flex-start">
+                                <Image src={signupHome} alt="home" h="100px" w="100px" minH="84px" />
+                            </Box>
+                            <Box mt="10px" maxW="80%" w="80%">
+                                <Text fontWeight="600" fontSize="18px" mb="8px">Login/Sign up</Text>
+                                <List spacing={3}>
+                                    <ListItem>
+                                        <ListIcon as={BsCheck} color='green.500' />
+                                        Zero Brokerage.
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListIcon as={BsCheck} color='green.500' />
+                                        Thousands of new listings daily.
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListIcon as={BsCheck} color='green.500' />
+                                        100 Cr+ Brokerage saved monthly.
+                                    </ListItem>
+                                </List>
+                            </Box>
+                        </Flex>
+                    </Flex>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
+
+export default LoginSignup
